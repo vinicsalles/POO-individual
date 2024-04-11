0219a1b8668291cd2c20ed58edd5acb6dc644c64
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-11 18:23:16 -0300

    fix: alterando Pessoa service

--- src/br/com/poo/projetoindividual/services/PessoaService.java
+++ src/br/com/poo/projetoindividual/services/PessoaService.java
@@ -1,5 +1,24 @@
 package br.com.poo.projetoindividual.services;
 
-public class PessoaService {
+import java.util.ArrayList;
+import java.util.List;
 
+import br.com.poo.projetoindividual.entities.Pessoa;
+
+public class PessoaService {
+	
+	
+	public List<String> listaNomesPessoas(){
+		//Croa uma instância de lista vazia do tipo String
+		List<String> listaNomesPessoas = new ArrayList<>();
+		//Cria um foreach para percorrer o mapa de Pessoas
+		for(Pessoa pessoa : Pessoa.getMapaPessoas().values()) {
+			//Adiciona o nome do pessoa na lista ListaNomePessoas
+			listaNomesPessoas.add(pessoa.getNome());
+			
+		}
+		return listaNomesPessoas;
+	}
+	
+	
 }
