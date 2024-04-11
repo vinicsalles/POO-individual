ea00ca653dccb76aa6bac5d2fdf34118b174b939
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-11 18:24:00 -0300

    fix: alterando application

--- src/br/com/poo/projetoindividual/projetoindividualApplication.java
+++ src/br/com/poo/projetoindividual/projetoindividualApplication.java
@@ -2,15 +2,17 @@ package br.com.poo.projetoindividual;
 
 import java.io.IOException;
 
-import br.com.poo.projetoindividual.controllers.CarroController;
 import br.com.poo.projetoindividual.io.RelatorioIO;
+import br.com.poo.projetoindividual.services.PessoaService;
 
 
 
 public class projetoindividualApplication {
 
 	public static void main(String[] args) throws IOException {
-			
+		PessoaService pessoa = new PessoaService();
+			RelatorioIO.leitor("banco");
+			RelatorioIO.listarNomesPessoas(pessoa.listaNomesPessoas());
 	}
 
 }
