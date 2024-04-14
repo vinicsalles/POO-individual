5e7b10e39b5a1bb6debb4f77feb6c3a53421583c
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-14 00:35:47 -0300

    fix: novos metodos criados

--- src/br/com/poo/projetoindividual/services/EnderecoService.java
+++ src/br/com/poo/projetoindividual/services/EnderecoService.java
@@ -1,5 +1,20 @@
 package br.com.poo.projetoindividual.services;
 
-public class EnderecoService {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.poo.projetoindividual.entities.Endereco;
+import br.com.poo.projetoindividual.io.RelatorioIO;
 
+public class EnderecoService {
+	public List<Endereco> listarEnd() throws IOException {
+		List<Endereco> listaEnderecos = new ArrayList<>();
+		for(Endereco end : Endereco.getMapaEnderecos().values()) {
+			listaEnderecos.add(end);
+		}
+		RelatorioIO.listarEnderecos(listaEnderecos);
+		return listaEnderecos;
+		
+	}
+	
 }
