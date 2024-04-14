be7039abe2e109166c1864bf3ec411ade38f0384
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-14 00:36:56 -0300

    fix: chamando metodo menu no application

--- src/br/com/poo/projetoindividual/projetoindividualApplication.java
+++ src/br/com/poo/projetoindividual/projetoindividualApplication.java
@@ -3,16 +3,14 @@ package br.com.poo.projetoindividual;
 import java.io.IOException;
 
 import br.com.poo.projetoindividual.io.RelatorioIO;
-import br.com.poo.projetoindividual.services.PessoaService;
-
-
+import br.com.poo.projetoindividual.services.MenuService;
 
 public class projetoindividualApplication {
 
 	public static void main(String[] args) throws IOException {
-		PessoaService pessoa = new PessoaService();
 			RelatorioIO.leitor("banco");
-			RelatorioIO.listarNomesPessoas(pessoa.listaNomesPessoas());
+			MenuService.menu();
+			
 	}
 
 }
