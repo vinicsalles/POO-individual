9e5b07c9e2fe37a4653aa0a2b1ca079a4b8564ea
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-14 00:37:28 -0300

    fix: Chamando service no controller

--- src/br/com/poo/projetoindividual/controllers/EnderecoController.java
+++ src/br/com/poo/projetoindividual/controllers/EnderecoController.java
@@ -1,5 +1,20 @@
 package br.com.poo.projetoindividual.controllers;
 
-public class EnderecoController {
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import br.com.poo.projetoindividual.entities.Endereco;
+import br.com.poo.projetoindividual.services.EnderecoService;
+import br.com.poo.projetoindividual.util.Util;
+
+public class EnderecoController {
+	EnderecoService enderecoService = new EnderecoService();
+	static Logger logger = Util.setupLogger();
+	public void listarEnderecos() throws IOException {
+		List<Endereco> end = enderecoService.listarEnd();
+		Util.customizer();
+		logger.log(Level.INFO, end + "\n\nLista gerada com Sucesso!!");
+	}
 }
