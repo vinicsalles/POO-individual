704776f167d5fe54cd3dd95aea236d850c737ad0
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-14 00:35:52 -0300

    fix: novos metodos criados

--- src/br/com/poo/projetoindividual/services/CarroService.java
+++ src/br/com/poo/projetoindividual/services/CarroService.java
@@ -1,26 +1,22 @@
 package br.com.poo.projetoindividual.services;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.poo.projetoindividual.entities.Carro;
+import br.com.poo.projetoindividual.io.RelatorioIO;
 
 public class CarroService {
-	Carro carro = new Carro();
 	
-	
-	public void listarCarro() {
-		carro.setNome("Supra");
-		carro.setMarca("Toyota");
-		carro.setModelo("Sedã");
-		carro.setPlaca("KTR8732");
-		carro.setAnoFabricacao(2020);
-		carro.setValor(10.0000);
-		
-		System.out.println(
-				carro.getNome()+ "\n"+
-				carro.getMarca()+ "\n"+
-				carro.getModelo()+ "\n"+
-				carro.getPlaca()+ "\n"+
-				carro.getAnoFabricacao()+ "\n"+
-				String.format("%.3f", carro.getValor())
-				);
+	public List<Carro> listarCarros() throws IOException {
+		List<Carro> listaCarros = new ArrayList<>();
+		for(Carro carro : Carro.getMapaCarros().values()) {
+			listaCarros.add(carro);
+		}
+		RelatorioIO.listarNomesCarros(listaCarros);
+		return listaCarros;
 	}
+
+	
 }
