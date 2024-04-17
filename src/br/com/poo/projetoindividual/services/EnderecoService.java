4ca6711ff51fa1ddac85d0b2da74e366ff959fd2
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-17 02:08:19 -0300

    fix: modificando enderecotxt

--- src/br/com/poo/projetoindividual/services/EnderecoService.java
+++ src/br/com/poo/projetoindividual/services/EnderecoService.java
@@ -17,9 +17,6 @@ public class EnderecoService {
 	
 	public List<Endereco> listarEndTXT() throws IOException {
 		List<Endereco> listaEnderecos = new ArrayList<>();
-		for(Endereco end : Endereco.getMapaEnderecos().values()) {
-			listaEnderecos.add(end);
-		}
 		RelatorioIO.listarEnderecos(listaEnderecos);
 		return listaEnderecos;
 		
