8ec383b2c5c2d34c8a319baac5dcadec17b9f209
Vinícius Salles <vsmiranda4321@gmail.com>
2024-04-17 02:05:59 -0300

    fix: adicionando thread.sleep

--- src/br/com/poo/projetoindividual/services/SubMenuService.java
+++ src/br/com/poo/projetoindividual/services/SubMenuService.java
@@ -1,6 +1,7 @@
 package br.com.poo.projetoindividual.services;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,124 +15,158 @@ public class SubMenuService {
 	static Logger logger = Util.setupLogger();
 	static Scanner sc = new Scanner(System.in);
 
-	public static void submenuConsole() throws IOException {
+	public static void submenuConsole() throws IOException, InterruptedException {
 		PessoaController pessoaController = new PessoaController();
 		CarroController carroController = new CarroController();
 		EnderecoController enderecoController = new EnderecoController();
 		Util.customizer();
 
-		logger.log(Level.INFO, () -> "\n\n ------------------Gerador de Relatórios:------------------\n\n" + "\t\t[1] Gerar lista de Pessoas\n" + "\t\t[2] Gerar lista de Carros\n"
-				+ "\t\t[3] Gerar lista de Endereços\n"+ "\t\t[4] Gerar lista de Pessoas e Endereços\n" + "\t\t[0] Sair\n" + "\n\n----------------------------------------------------------- " + "\n\n\t\tDigite uma opção:");
+		logger.log(Level.INFO,
+				() -> "\n\n ------------------Gerador de Relatórios:------------------\n\n"
+						+ "\t\t[1] Gerar lista de Pessoas\n" + "\t\t[2] Gerar lista de Carros\n"
+						+ "\t\t[3] Gerar lista de Endereços\n" + "\t\t[4] Gerar lista de Pessoas e Endereços\n"
+						+ "\t\t[0] Voltar\n" + "\n\n----------------------------------------------------------- "
+						+ "\n\n\t\tDigite uma opção:");
+		try {
+			int opcao = sc.nextInt();
 
-		int opcao = sc.nextInt();
+			switch (opcao) {
 
-		switch (opcao) {
+			case 1:
+				Thread.sleep(1000);
+				logger.log(Level.INFO, () -> "Lista os nomes das Pessoas:\n ");
+				pessoaController.listarNomePessoas();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuConsole();
 
-		case 1:
-			logger.log(Level.INFO, () -> "Lista os nomes das Pessoas:\n ");
-			pessoaController.listarNomePessoas();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuConsole();
-			
-			break;
+				break;
 
-		case 2:
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista os Carros:\n ");
-			carroController.listarCarros();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuConsole();
+			case 2:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista os Carros:\n ");
+				carroController.listarCarros();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuConsole();
 
-			break;
+				break;
 
-		case 3:
+			case 3:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista de Endereços:\n ");
+				enderecoController.listarEnderecos();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuConsole();
+				break;
 
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista de Endereços:\n ");
-			enderecoController.listarEnderecos();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuConsole();
-			break;
-			
-		case 4:
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista os Pessoas e Endereços:\n ");
-			pessoaController.listarPessoEnd();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuConsole();
-		
-			break;
-			
-		case 0:
-			Util.customizer();
-			MenuService.menu();
-			break;
+			case 4:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista os Pessoas e Endereços:\n ");
+				pessoaController.listarPessoEnd();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuConsole();
 
-		default:
+				break;
+
+			case 0:
+				Thread.sleep(1000);
+				Util.customizer();
+				MenuService.menu();
+				break;
+
+			default:
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Opção Inválida");
+				submenuConsole();
+				break;
+			}
+		} catch (InputMismatchException e) {
 			Util.customizer();
 			logger.log(Level.INFO, () -> "Opção Inválida");
-			submenuConsole();
-			break;
+			sc.nextLine();
+			Thread.sleep(1000);
+			SubMenuService.submenuConsole();
 		}
 	}
-	public static void submenuTXT() throws IOException {
+
+	public static void submenuTXT() throws IOException, InterruptedException {
 		PessoaController pessoaController = new PessoaController();
 		CarroController carroController = new CarroController();
 		EnderecoController enderecoController = new EnderecoController();
 		Util.customizer();
 
-		logger.log(Level.INFO, () -> "\n\n ------------------Gerador de Relatórios:------------------\n\n" + "\t\t[1] Gerar lista de Pessoas\n" + "\t\t[2] Gerar lista de Carros\n"
-				+ "\t\t[3] Gerar lista de Endereços\n"+ "\t\t[4] Gerar lista de Pessoas e Endereços\n" + "\t\t[0] Sair\n" + "\n\n----------------------------------------------------------- " + "\n\n\t\tDigite uma opção:");
+		logger.log(Level.INFO,
+				() -> "\n\n ------------------Gerador de Relatórios:------------------\n\n"
+						+ "\t\t[1] Gerar lista de Pessoas\n" + "\t\t[2] Gerar lista de Carros\n"
+						+ "\t\t[3] Gerar lista de Endereços\n" + "\t\t[4] Gerar lista de Pessoas e Endereços\n"
+						+ "\t\t[0] Voltar\n" + "\n\n----------------------------------------------------------- "
+						+ "\n\n\t\tDigite uma opção:");
 
-		int opcao = sc.nextInt();
+		try {
+			int opcao = sc.nextInt();
 
-		switch (opcao) {
+			switch (opcao) {
 
-		case 1:
-			logger.log(Level.INFO, () -> "Lista TXT De nomes Criada com Sucesso!!:\n ");
-			pessoaController.listarNomesPessoasTXT();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuTXT();
-			
-			break;
+			case 1:
+				Thread.sleep(1000);
+				logger.log(Level.INFO, () -> "Lista TXT De nomes Criada com Sucesso!!:\n ");
+				pessoaController.listarNomesPessoasTXT();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuTXT();
 
-		case 2:
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista os Carros:\n ");
-			carroController.listarCarrosTXT();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuTXT();
+				break;
 
-			break;
+			case 2:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista os Carros:\n ");
+				carroController.listarCarrosTXT();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuTXT();
 
-		case 3:
+				break;
 
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista de Endereços:\n ");
-			enderecoController.listarEnderecosTXT();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuTXT();
-			break;
-			
-		case 4:
-			Util.customizer();
-			logger.log(Level.INFO, () -> "Lista os Pessoas e Endereços:\n ");
-			pessoaController.listarPessoEnd();
-			logger.log(Level.INFO, () -> "\n\n");
-			submenuTXT();
-		
-			break;
-			
-		case 0:
-			Util.customizer();
-			MenuService.menu();
-			break;
+			case 3:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista de Endereços:\n ");
+				enderecoController.listarEnderecosTXT();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuTXT();
+				break;
 
-		default:
+			case 4:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Lista os Pessoas e Endereços:\n ");
+				pessoaController.listarPessoEndTXT();
+				logger.log(Level.INFO, () -> "\n\n");
+				submenuTXT();
+
+				break;
+
+			case 0:
+				Thread.sleep(1000);
+				Util.customizer();
+				MenuService.menu();
+				break;
+
+			default:
+				Thread.sleep(1000);
+				Util.customizer();
+				logger.log(Level.INFO, () -> "Opção Inválida");
+				submenuTXT();
+				break;
+			}
+		} catch (InputMismatchException e) {
+			Thread.sleep(1000);
 			Util.customizer();
 			logger.log(Level.INFO, () -> "Opção Inválida");
-			submenuTXT();
-			break;
+			sc.nextLine();
+			Thread.sleep(1000);
+			SubMenuService.submenuTXT();
 		}
 	}
 }
